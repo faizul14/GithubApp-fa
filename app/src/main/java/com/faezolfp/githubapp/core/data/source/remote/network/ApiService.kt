b3494d179a9e5f8a18314de3c531d8ea76348e89
Faezol <fpcyber61@gmail.com>
2023-10-14 16:08:22 +0800

    fix: change auth public

--- app/src/main/java/com/faezolfp/githubapp/core/data/source/remote/network/ApiService.kt
+++ app/src/main/java/com/faezolfp/githubapp/core/data/source/remote/network/ApiService.kt
@@ -10,23 +10,23 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("users")
     suspend fun getDataUsers(): List<ResponseDataUserItem>
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("users/{login}")
     suspend fun getDetailUser(
         @Path("login") login: String
     ): ResponseDetailUser
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("users/{login}/repos")
     suspend fun getRepoUser(
         @Path("login") login: String
     ): List<ResponseRepoUserItem>
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("search/users")
     suspend fun searchUser(
         @Query("q") login: String
